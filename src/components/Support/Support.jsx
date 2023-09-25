1212eb2b611687186e1c47f3d805297d7ed84a79
Sharifa Abdirizak <sharifamabdirizak@gmail.com>
2023-09-25 07:50:31 -0500

    added imports to support.jsx.

--- src/components/Support/Support.jsx
+++ src/components/Support/Support.jsx
@@ -1,5 +1,20 @@
+import React, { useState }  from 'react';
+import { useHistory } from 'react-router-dom';
+import { useDispatch } from 'react-redux'
+import axios from "axios";
+
+
+function Understanding() {
+    const history = useHistory();
+    const dispatch = useDispatch();
+    const [support, setSupport] = useState(0);
+    
+
+
 function Support (){
 
+
+    
 }
 
 export default Support;
--- src/components/Understanding/Understanding.jsx
+++ src/components/Understanding/Understanding.jsx
@@ -35,6 +35,8 @@ function Understanding() {
             </div>
         </>
     );
-}
+    }
+
+
 
 export default Understanding;
