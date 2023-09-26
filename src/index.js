657742496618c224a6a8dd3cc4a9111ab84a4b9f
Sharifa Abdirizak <sharifamabdirizak@gmail.com>
2023-09-26 08:33:54 -0500

    changed formating and error in my code

--- src/index.js
+++ src/index.js
@@ -8,9 +8,7 @@ import { applyMiddleware, combineReducers, createStore } from "redux";
 import { Provider } from 'react-redux'; 
 
 const feedbackAnswers = (state = {}, action) => {
-  //going to use a switch here because i think i'm going to end up only using one reducer? we'll see
-  //but if end up using only one reducer, i'll have 4 if statements and that's going to be hard to read/bulky
-  //a switch is in this case for better readability
+ 
   switch (action.type) {
       case 'ADD_FEELINGS':
           return { ...state, feeling: action.payload };
@@ -21,18 +19,17 @@ const feedbackAnswers = (state = {}, action) => {
       case 'ADD_COMMENT':
           return { ...state, comments: action.payload };
       case 'RESET_FEEDBACK':
-          //return an empty array when starting over the loop
-          return {};
+          
       default:
           return state;
   }
 }
 const reduxStore = createStore(
   combineReducers({
-      //sending over feebackAnswers to use in react app
+     
       feedbackAnswers,
   }),
-  //this will show us previous state, the action, and next state in the console on the DOM
+
   applyMiddleware(logger)
 );
 
